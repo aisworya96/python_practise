6a4450a97f633b735fe57cb917fec1382df81687
apple <paisworya2@gmail.com>
2024-06-24 17:53:03 +0530

    Add files

--- lists/practise1.py
+++ lists/practise1.py
@@ -1,4 +1,8 @@
-list = ["apple", "mango", "cherry"]
-list[2] = "banana"
-print(list)
-print(len(list))
+# list = ["apple", "mango", "cherry"]
+# list[2] = "banana"
+# print(list)
+# print(len(list))
+
+# convert string to a list
+str = "Aisworya Panda"
+print(str.split(" "))
---
+++ lists/reverse_str.py
@@ -0,0 +1,5 @@
+str ="Aisworya Panda"
+str1 =""
+for i in str:
+    str1 = i + str1
+print("The reversed string: ", str1)
